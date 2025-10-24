e82863f0c651995fc73f0ead53247b88f4b2ab82
Florian Migge <3029804@stud.hs-mannheim.de>
2025-10-24 20:17:47 +0200

    Private Änderung

---
+++ Profil.java
@@ -0,0 +1,10 @@
+public class Profil {
+    public static void main(String[] args){
+        String meinName = "Florian";
+        int matrikelnummer = 007;
+
+        System.out.println("Mein Name ist = " + meinName + " und meine Matrikelnummer ist = " + matrikelnummer);
+
+
+    }
+}
